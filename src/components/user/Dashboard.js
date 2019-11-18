2dc9873c9fdf0c1a78262ea53ecbb49e28a5fe5b
M S Vikas Kumar <msvikaskumar@BLR1-LMAC-N60813.local>
2019-11-18 22:11:50 +0530

    minor modifications

--- src/components/user/Dashboard.js
+++ src/components/user/Dashboard.js
@@ -6,18 +6,22 @@ import BarChart from '../../assets/Charts/BarChart';
 
 const Dashboard = (props) => {
 
-    const getDtPoints = () =>{
+    const getVDtPoints = () =>{
         let dt_array = [['On-Road', 5], ['Idle', 6], ['Maintenance', 7]]
+        return dt_array.map(([key, val]) => ({label: key, y: val}))
+    }
+    const getDDtPoints = () =>{
+        let dt_array = [['On-duty', 4], ['Idle', 2], ['Off-Duty', 1]]
         return dt_array.map(([key, val]) => ({label: key, y: val}))
     }
     return (
         <div style={{display: "flex", marginTop: "15%"}}>
         {console.log(props.user_info)}
         <div style={{marginLeft: "5%", width: "30%"}}>
-        <BarChart title = "Vehicles" dtpoints = {getDtPoints()}/>
+        <BarChart title = "Vehicles" dtpoints = {getVDtPoints()}/>
         </div>
         <div style={{marginLeft: "25%", width: "35%"}}>
-        <BarChart title = "Drivers" dtpoints = {getDtPoints()}/>
+        <BarChart title = "Drivers" dtpoints = {getDDtPoints()}/>
         </div>
       </div>
     )
